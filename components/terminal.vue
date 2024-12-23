556540c52b36bb99f3efcb0efb68c3431640f2dc
Edison Santiago <edisonluizsm@gmail.com>
2024-12-23 08:45:58 -0300

    Fix ts errors

--- components/terminal.vue
+++ components/terminal.vue
@@ -2,7 +2,7 @@
   <div class="flex-grow overflow-y-scroll max-h-screen text-terminal-green font-noto-mono leading-normal p-2" @click="focusTerminalInput">
     <ul class="max-w-prose">
       <li v-for="text in defaultTexts">
-        <i18n-t :keypath="text" tag="p">
+        <i18n-t :keypath="text" tag="p" scope="global">
           <template v-if="text === 'currentWork'" v-slot:stapp>
             <a class="underline cursor-pointer" href="https://stapp.studio" target="_blank" rel="noopener noreferrer">Stapp Studio</a>
           </template>
@@ -11,31 +11,44 @@
       </li>
       <li>{{ $t('typeACommand')}}</li>
       <li>{{ $t('commandList')}}</li>
-      <li>> {{ inputData }}<span class="blink">█</span></li>
+      <li v-for="line in terminalLines">
+        {{ line }}
+      </li>
+      <li>{{ `user@${hostname}` }} > {{ currentInputData }}<span class="blink">█</span></li>
     </ul>
-    <input id="terminalInput" v-model="inputData" class="bg-white opacity-0" @keypress.enter="enterPressed">
+    <input id="terminalInput" v-model="currentInputData" class="bg-white opacity-0" @keypress.enter="enterPressed">
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const { t } = useI18n()
+const hostname = computed(() => useRequestURL().hostname)
+
 onMounted(() => {
   focusTerminalInput();
 })
-
-const inputData = ref('')
 
 const defaultTexts = computed(() => [
   'hello', 'whoAmI', 'whatIDo', 'currentWork'
 ])
 
+const currentInputData = ref('')
+
 function focusTerminalInput() {
-  document.getElementById('terminalInput').focus();
+  document.getElementById('terminalInput')?.focus();
 }
 
 function enterPressed() {
-  alert('enter pressed!')
+  const command = currentInputData.value;
+  currentInputData.value = '';
+  terminalLines.value.push('> ' + command);
+
+  if (command == '') return
+
+  terminalLines.value.push(hostname.value + ': command not found: ' + command)
 }
+
+const terminalLines: Ref<string[]> = ref([])
 
 </script>
 
